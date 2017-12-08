b183d746fd8464d77b689a0d80a900de0afb3b20
ShanGao5 <31430282+ShanGao5@users.noreply.github.com>
2017-12-08 00:35:44 -0800

    Update AssessFitness.R
    Changes made by Kexin Fei, Shan Gao

--- R/AssessFitness.R
+++ R/AssessFitness.R
@@ -19,23 +19,28 @@
 # of the LS fit. The default for determining fitness is the Aikake Criteria Index
 # but the user can supply their own custom-made fitness function
 # **may be worth it to treat 'predictors' as global variable or object
-AssessFitness <- function(individual, response, user.family="gaussian", predictors, userfunc=FALSE){
 
+AssessFitness <- function(individual, response, user.family="gaussian", predictors, userfunc=FALSE){
+  
   #Evaluate the fitness of some model, output from glm
   #The userfunc should take a fitted model and output a scalar fitness value
-
+  
+  #if model selects one or zero predictors, then add 2 predictors to the model
+  if((sum(individual) == 0) || (sum(individual) == 1)){
+    individual[sample(1:length(individual), 2)] = 1
+  }
+  
   predictors.individual <- predictors[,individual==1]
-
+  
   #Check distribution family of glm()
   if(!user.family %in% c("binomial", "gaussian", "Gamma", "poisson", "inverse.gaussian")){
-    print(paste("WARNING: User defined distribution family ", user.family, " does not exist", sep=''))
-    stop()
-    geterrmessage()
+    print(paste("WARNING: User defined distribution family ", user.family, " is not existed", sep=''))
+    fitness.value = NULL
   }
   else{
     model.out <- glm(response[,1]~., family=user.family, predictors.individual)
-    #model.out <- lm(response[,1]~., predictors.individual)
     fitness.value <- FitnessFunction(model.out, userfunc)
   }
   return(fitness.value)
 }
+

81822c473f34ee9ded4d4bd199a042e8b85536ab
ShanGao5 <31430282+ShanGao5@users.noreply.github.com>
2017-12-08 00:42:35 -0800

    Update ExtractResponseVariable.R
    Changes made by Kexin Fei, Yachen Wang, Shan Gao

--- R/ExtractResponseVariable.R
+++ R/ExtractResponseVariable.R
@@ -8,31 +8,39 @@
 #' @export
 #' @examples
 #' ExtractResponseVariable()
+# function to get separate response variable from predictors
 
+#Changes made by Kexin Fei, Yachen Wang, Shan Gao
 ExtractResponseVariable <- function(dataset,name) {
-
   #Takes a dataframe, dataset and a name of a response variable
-  #Extracts the response variable and dataframe of predictors, outputs these as members
+  #Extracts the response variable and dataframe of predictors, outputs these as members 
   #of a list
-  if (any(is.na(dataset[,1]))==TRUE){
-    print("The response variable has missing values")
-    dataset <- dataset[!is.na(dataset[,1])]
+  
+  if(sum(!sapply(dataset[, -which(names(dataset)==name)], class %in% c("numeric", "integer"))>0)){
+    print("WARNING: There exists certain non-numeric predictor(s)")
+    stop()
+    geterrmessage()
   }
-  else{
-    if (name %in% colnames(dataset)) {
-
-      name <- as.character(name)
-
-      #Get matrix of predictors
-      predictors <- dataset
-      predictors[name] <- NULL
-
-      #Get response variable
-      response <- dataset[name]
-      return(list(response,predictors))
-    } else {
-      print(paste("Name ",name," not found in dataset",sep=''))
-      return(list(0L,0L))
+  
+  #if there are missing values in response variable, remove the observation and proceed the function
+  if (any(is.na(dataset[name]))){
+      print("The response variable has missing values")
+      dataset <- dataset[!is.na(dataset[name])]
     }
+  
+  if (name %in% colnames(dataset)) {
+    #Get matrix of predictors
+    predictors <- dataset
+    predictors[name] <- NULL
+        
+    #Get response variable
+    response <- dataset[name]
+    return(list(response,predictors))
+  } 
+  
+  else {
+    print(paste("Name ",name," not found in dataset",sep=''))
+    return(list(0L,0L))
   }
 }
+

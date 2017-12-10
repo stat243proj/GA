a8b8dbaaffb91692c1a4c21e3b725642231245f7
echo-orange <31430464+echo-orange@users.noreply.github.com>
2017-12-10 15:03:20 -0800

    Update Test_ReplaceClones_Function.R

--- tests/testthat/Test_ReplaceClones_Function.R
+++ tests/testthat/Test_ReplaceClones_Function.R
@@ -19,3 +19,22 @@ test_that("Check the output of the ReplaceClones function is correct",{
                                 rnorm(40, 700, 1), 27)$generation, 40)
   })
 
+test_that("Check the output of the ReplaceClones function with mtcars data is correct",{
+    set.seed(2)
+  fitness.vec <- rnorm(16, 500, 20)
+    expect_equal(ReplaceClones(lapply(1:16, function(x) {rep(1, times=10)}), 
+                                     fitness.vec, 10)$generation[[1]], 
+                 c(1, 1, 1, 1, 1, 1, 1, 1, 1, 1))
+    
+    expect_equal(ReplaceClones(lapply(1:16, function(x) {rep(NA, times=10)}), 
+                                     fitness.vec, 10)$generation[[1]], 
+                 c(NA, NA, NA, NA, NA, NA, NA, NA, NA, NA))
+    
+  })
+
+test_that("check the dimension of return generation from ReplaceClones function is correct",{
+    expect_length(ReplaceClones(lapply(1:16, function(x) {rep(1, times=10)}),
+                                rnorm(16, 500, 20), 10)$generation, 16)
+  })
+
+

845b19ccc52cf52fe0c05015b6c007c034f8c113
echo-orange <31430464+echo-orange@users.noreply.github.com>
2017-12-09 19:55:00 -0800

    Update Test_Breed_Function.R

--- tests/testthat/Test_Breed_Function.R
+++ tests/testthat/Test_Breed_Function.R
@@ -4,19 +4,21 @@ context("Test the dimension of the output of Breed function")
 test_Breed <- function(dataset, response.name){
   require(testthat)
   
+  subsets <- ExtractResponseVariable(dataset, response.name)
+  predictors <- subsets[[2]]
   C <- length(predictors)
   Niter <<- Niter
-  P <<- as.integer(C*1.5)
-  predictors <- dataset
+  P <- as.integer(C*1.5)
+  P <<- 2*ceiling(P/2)
   prob.mutate <- 1.0/(P*sqrt(C))
   generation.old <- lapply(1:P, function(x) {rbinom(C,1,0.5)})
   fitness <- matrix(0,P,Niter)
   
   test_that("Check the dimension of return list from Breed function is P embeded lists, 
             with length of each element(child genome) is C",{
-              expect_equal(length(Breed(generation.old, fitness[,1], predictors, prob.mutate)), P/2)
+              expect_equal(length(Breed(generation.old, fitness[,1], prob.mutate)), P-1)
               expect_equal(unique(unlist(
-                lapply(Breed(generation.old, fitness[,1], predictors, prob.mutate),function(x) length(unlist(x))
-                ))), 2*C)
+                lapply(Breed(generation.old, fitness[,1], prob.mutate),function(x) length(unlist(x))
+                ))), C)
 })
 }

f74b37ab9ba34a5047f7b858d6677a72f51295c5
echo-orange <31430464+echo-orange@users.noreply.github.com>
2017-12-10 13:04:58 -0800

    Update Test_ReplaceClones_Function.R

--- tests/testthat/Test_ReplaceClones_Function.R
+++ tests/testthat/Test_ReplaceClones_Function.R
@@ -6,6 +6,11 @@ test_that("Check the output of the ReplaceClones function is correct",{
     expect_equal(ReplaceClones(lapply(1:40, function(x) {rep(1, times=27)}), 
                                      matrix(0,40,100), 27)$generation[[1]], 
                  c(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1))
+    
+    expect_equal(ReplaceClones(lapply(1:40, function(x) {rep(NA, times=27)}), 
+                                     matrix(0,40,100), 27)$generation[[1]], 
+                 c(NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA))
+    
   })
 
   test_that("check the dimension of return generation from ReplaceClones function is correct",{

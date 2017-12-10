c4493c8c9a88132fa208631ed0960d2b1a16b0fe
echo-orange <31430464+echo-orange@users.noreply.github.com>
2017-12-10 12:17:46 -0800

    Update Test_CrossOverMutate_Function.R

--- tests/testthat/Test_CrossOverMutate_Function.R
+++ tests/testthat/Test_CrossOverMutate_Function.R
@@ -8,6 +8,12 @@ context("Test the dimension of the output of CrossOverMutate function")
     expect_equal(CrossOverMutate(generation, c(1,2), 0.004811252)[[1]], 
     c(0, 1, 1, 0, 0, 1, 0, 1, 1, 1, 0, 1, 0, 0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1))
   })
+
+test_that("Check the value of return value from CrossOverMutate function with a zero list is correct",{
+  expect_equal(CrossOverMutate(lapply(1:40, function(x) {rep(0, times=27)}), 
+                                 c(1,2), 0.004811252)[[1]], 
+    c(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0))
+  })
   
   test_that("Check the dimension of return value(a child genome) from CrossOverMutate function is correct",{
     set.seed(1)

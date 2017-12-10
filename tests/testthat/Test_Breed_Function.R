6eaad5a386201b506b2c81ade29c47799d1f4abf
echo-orange <31430464+echo-orange@users.noreply.github.com>
2017-12-10 11:43:18 -0800

    Update Test_Breed_Function.R

--- tests/testthat/Test_Breed_Function.R
+++ tests/testthat/Test_Breed_Function.R
@@ -1,27 +1,16 @@
 #Test the Breed function
-context("Test the dimension of the output of Breed function")
+context("Test the dimension and value of the output of Breed function")
 
-test_Breed <- function(dataset, response.name){
-  require(testthat)
-  
-  subsets <- ExtractResponseVariable(dataset, response.name)
-  predictors <- subsets[[2]]
-  C <- length(predictors)
-  Niter <<- Niter
-  P <- as.integer(C*1.5)
-  P <<- 2*ceiling(P/2)
-  prob.mutate <- 1.0/(P*sqrt(C))
-  set.seed(1)
-  generation.old <- lapply(1:P, function(x) {rbinom(C,1,0.5)})
-  fitness <- matrix(0,P,Niter)
-  
-  test_that("Check the dimension of return list from Breed function is P embeded lists, 
-            with length of each element(child genome) is C",{
-              expect_equal(length(Breed(generation.old, fitness[,1], prob.mutate)), P-1)
+test_that("With the baseball data, check the dimension of return list from Breed function is 39 embeded lists, 
+            with length of each element(child genome) is 27",{
+              set.seed(1)
+              generation <- lapply(1:40, function(x) {rbinom(C, 1, 0.5)})
+              set.seed(1)
+              fitness.vec <- rnorm(40, 700, 1)
+              expect_equal(length(Breed(generation, fitness.vec, 0.004811252)), 39)
               expect_equal(unique(unlist(
-                lapply(Breed(generation.old, fitness[,1], prob.mutate),function(x) length(unlist(x))
-                ))), C)
-              expect_equal(Breed(generation.old, fitness[,1], prob.mutate)[[1]], 
-               c(0, 1, 1, 0, 0, 1, 0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1))
+                lapply(Breed(generation, fitness.vec, 0.004811252),function(x) length(unlist(x))
+                ))), 27)
+              expect_equal(Breed(generation, fitness.vec, 0.004811252)[[1]], 
+               c(0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 0))
 })
-}

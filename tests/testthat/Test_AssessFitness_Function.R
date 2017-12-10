75c4f415584790d6ff6007beedf3f18aa2844157
echo-orange <31430464+echo-orange@users.noreply.github.com>
2017-12-10 14:04:32 -0800

    Update Test_AssessFitness_Function.R

--- tests/testthat/Test_AssessFitness_Function.R
+++ tests/testthat/Test_AssessFitness_Function.R
@@ -3,8 +3,6 @@
 context("Test the return value of AssessFitness function")
 
 test_that("Check the return value of AssessFitness function",{
-  set.seed(1)
-  individual <- rbinom(27, 1, prob=0.5)
-  expect_equal(round(AssessFitness(individual,response, predictors, 
-                               user.family = "gaussian", userfunc = "AIC")), 852)
+  expect_equal(round(AssessFitness(rep(1, times=27),response, predictors, 
+                               user.family = "gaussian", userfunc = "AIC")), 562)
   })

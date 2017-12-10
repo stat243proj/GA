3ef0c75f37a62fdb6dc2f66fce16cdd6aab28065
echo-orange <31430464+echo-orange@users.noreply.github.com>
2017-12-10 09:52:15 -0800

    Update Test_Breed_Function.R

--- tests/testthat/Test_Breed_Function.R
+++ tests/testthat/Test_Breed_Function.R
@@ -11,9 +11,9 @@ test_Breed <- function(dataset, response.name){
   P <- as.integer(C*1.5)
   P <<- 2*ceiling(P/2)
   prob.mutate <- 1.0/(P*sqrt(C))
+  set.seed(1)
   generation.old <- lapply(1:P, function(x) {rbinom(C,1,0.5)})
   fitness <- matrix(0,P,Niter)
-  set.seed(1)
   
   test_that("Check the dimension of return list from Breed function is P embeded lists, 
             with length of each element(child genome) is C",{
@@ -21,7 +21,6 @@ test_Breed <- function(dataset, response.name){
               expect_equal(unique(unlist(
                 lapply(Breed(generation.old, fitness[,1], prob.mutate),function(x) length(unlist(x))
                 ))), C)
-              set.seed(1)
               expect_equal(Breed(generation.old, fitness[,1], prob.mutate)[[1]], 
                c(0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0))
 })

fc92cda3aad63a1e0ab83e529231c93519b6dfc4
echo-orange <31430464+echo-orange@users.noreply.github.com>
2017-12-09 19:59:09 -0800

    Update Test_CrossOverMutate_Function.R

--- tests/testthat/Test_CrossOverMutate_Function.R
+++ tests/testthat/Test_CrossOverMutate_Function.R
@@ -4,10 +4,12 @@ context("Test the dimension of the output of CrossOverMutate function")
 test_CrossOverMutate <- function(dataset, response.name){
   require(testthat)
   
-  predictors <- dataset
+  subsets <- ExtractResponseVariable(dataset, response.name)
+  predictors <- subsets[[2]]
   C <- length(predictors)
   Niter <<- Niter
-  P <<- as.integer(C*1.5)
+  P <- as.integer(C*1.5)
+  P <<- 2*ceiling(P/2)
   prob.mutate <- 1.0/(P*sqrt(C))
   generation.old <- lapply(1:P, function(x) {rbinom(C,1,0.5)})
   

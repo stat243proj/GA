515fbb1db55772f6af8e2f59c027ea30dd4f7140
echo-orange <31430464+echo-orange@users.noreply.github.com>
2017-12-10 13:07:53 -0800

    Update Test_CrossOverMutate_Function.R

--- tests/testthat/Test_CrossOverMutate_Function.R
+++ tests/testthat/Test_CrossOverMutate_Function.R
@@ -3,10 +3,8 @@ context("Test the dimension of the output of CrossOverMutate function")
 
 #test CrossOverMutate function
  test_that("Check the value of return value(a child genome) from CrossOverMutate function is correct",{
-    set.seed(1)
-    generation <- lapply(1:40, function(x) {rbinom(27, 1, 0.5)})
-    expect_equal(CrossOverMutate(generation, c(1,2), 0.004811252)[[1]], 
-    c(0, 1, 1, 0, 0, 1, 0, 1, 1, 1, 0, 1, 0, 0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1))
+    expect_equal(CrossOverMutate(lapply(1:40, function(x) {rep(1, times=27)}), c(1,2), 0.004811252)[[1]], 
+    c(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1))
   })
 
 test_that("Check the value of return value from CrossOverMutate function with a zero list is correct",{

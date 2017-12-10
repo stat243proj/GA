53a12a72514fb2447b89dad89044999c51ce56c8
echo-orange <31430464+echo-orange@users.noreply.github.com>
2017-12-10 11:12:22 -0800

    Update Test_AssessFitness_Function.R

--- tests/testthat/Test_AssessFitness_Function.R
+++ tests/testthat/Test_AssessFitness_Function.R
@@ -1,16 +1,10 @@
 #Test the AssessFitness function
-context("Test the type of return value of AssessFitness function")
 
-test_AssessFitness <- function(dataset, response.name){
-  require(testthat)
-  
-  subsets <- ExtractResponseVariable(dataset, response.name)
-  predictors <- subsets[[2]]
+context("Test the return value of AssessFitness function")
+
+test_that("Check the return value of AssessFitness function",{
   set.seed(1)
-  generation.old <- lapply(1, function(x) {rbinom(C, 1, 0.5)})
-  
-  test_that("Check type of return value of AssessFitness function",{
-    set.seed(1)
-    expect_equal(round(sapply(generation.old, AssessFitness, response = response, predictors = predictors)), 5536)
+  individual <- rbinom(27, 1, prob=0.5)
+  expect_equal(round(AssessFitness(individual,response, predictors, 
+                               user.family = "gaussian", userfunc = "AIC")), 852)
   })
-}

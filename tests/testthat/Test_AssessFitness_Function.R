7224342d4f6135cd40f7c7f6d564725c55d379e7
echo-orange <31430464+echo-orange@users.noreply.github.com>
2017-12-10 09:48:03 -0800

    Update Test_AssessFitness_Function.R

--- tests/testthat/Test_AssessFitness_Function.R
+++ tests/testthat/Test_AssessFitness_Function.R
@@ -6,11 +6,11 @@ test_AssessFitness <- function(dataset, response.name){
   
   subsets <- ExtractResponseVariable(dataset, response.name)
   predictors <- subsets[[2]]
+  set.seed(1)
+  generation.old <- lapply(1, function(x) {rbinom(C, 1, 0.5)})
   
   test_that("Check type of return value of AssessFitness function",{
-    expect_equal(class(
-      AssessFitness(individual=rep(1, dim(baseball.dat)[2]), response="salary", 
-                    predictors, user.family="gaussian", userfunc="AIC")
-    ), "numeric")
+    set.seed(1)
+    expect_equal(round(sapply(generation.old, AssessFitness, response = response, predictors = predictors)), 5536)
   })
 }

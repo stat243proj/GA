117b92db504b2cf1861333ee838e335d6350f3fa
echo-orange <31430464+echo-orange@users.noreply.github.com>
2017-12-10 14:15:21 -0800

    Update Test_AssessFitness_Function.R

--- tests/testthat/Test_AssessFitness_Function.R
+++ tests/testthat/Test_AssessFitness_Function.R
@@ -3,6 +3,12 @@
 context("Test the return value of AssessFitness function")
 
 test_that("Check the return value of AssessFitness function",{
-  expect_equal(round(AssessFitness(rep(1, times=27),response, predictors, 
-                               user.family = "gaussian", userfunc = "AIC")), 562)
+  expect_equal(round(AssessFitness(rep(1, times=27),baseball.dat["salary"], baseball.dat, 
+                               user.family = "gaussian", userfunc = "AIC")), -18653)
+  })
+
+test_that("Check the return value of AssessFitness function with mtcars data",{
+  data(mtcars)
+  expect_equal(round(AssessFitness(rep(1, times=10), mtcars["mpg"], mtcars, 
+                               user.family = "gaussian", userfunc = "AIC")), -2036)
   })

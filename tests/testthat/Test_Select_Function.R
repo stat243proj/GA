9cd8dde6cf180901494e114dde94a4c72def95f9
echo-orange <31430464+echo-orange@users.noreply.github.com>
2017-12-10 17:04:49 -0800

    Update Test_Select_Function.R

--- tests/testthat/Test_Select_Function.R
+++ tests/testthat/Test_Select_Function.R
@@ -1,7 +1,8 @@
 #Test the Select function
 context("Test the type of return value of Selection function")
 
-  #test Select function
+#test Select function
+#test Select function with baseball data
  test_that("Check the output of the Select function with baseball data is correct",{
   set.seed(7)
   output <- Select(baseball.dat, "salary", userfunc="AIC", user.family="gaussian", flag.log.scale=TRUE,
@@ -11,6 +12,15 @@ context("Test the type of return value of Selection function")
                  c(1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0))
   })
 
+test_that("Check the dimension of fitness matrix of the Select function with baseball data", {
+  set.seed(7)
+  output <- Select(baseball.dat, "salary", userfunc="AIC", user.family="gaussian", flag.log.scale=TRUE,
+                   frac.replace=0.2, Niter=100, mutate.rate=FALSE, plot.flag=TRUE)
+  expect_equal(dim(output$fitness)[1], 40)
+  expect_equal(dim(output$fitness)[2], 100)
+})
+
+#test Select function with mtcars data
 test_that("Check the output of the Select function with mtcars data is correct",{
   set.seed(8)
   result <- Select(dataset=cars, response.name="mpg", user.family="gaussian", flag.log.scale=TRUE, Niter = 50, frac.replace = 0.2, mutate.rate = FALSE)

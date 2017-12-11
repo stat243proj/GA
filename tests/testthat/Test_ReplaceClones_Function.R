c951dd15dc99bce0ff0a0512354bef9c07c972f1
echo-orange <31430464+echo-orange@users.noreply.github.com>
2017-12-10 16:07:25 -0800

    Update Test_ReplaceClones_Function.R

--- tests/testthat/Test_ReplaceClones_Function.R
+++ tests/testthat/Test_ReplaceClones_Function.R
@@ -5,36 +5,36 @@ test_that("Check the output of the ReplaceClones function is correct",{
     set.seed(1)
     fitness.vec <- rnorm(40, 700, 1)
     expect_equal(ReplaceClones(lapply(1:40, function(x) {rep(1, times=27)}), 
-                                     fitness.vec, 27)$generation[[1]], 
+                                     fitness.vec)$generation[[1]], 
                  c(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1))
     
     expect_equal(ReplaceClones(lapply(1:40, function(x) {rep(NA, times=27)}), 
-                                     fitness.vec, 27)$generation[[1]], 
+                                     fitness.vec)$generation[[1]], 
                  c(NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA))
     
   })
 
   test_that("check the dimension of return generation from ReplaceClones function is correct",{
     expect_length(ReplaceClones(lapply(1:40, function(x) {rep(1, times=27)}),
-                                rnorm(40, 700, 1), 27)$generation, 40)
+                                rnorm(40, 700, 1))$generation, 40)
   })
 
 test_that("Check the output of the ReplaceClones function with mtcars data is correct",{
     set.seed(2)
   fitness.vec <- rnorm(16, 500, 20)
     expect_equal(ReplaceClones(lapply(1:16, function(x) {rep(1, times=10)}), 
-                                     fitness.vec, 10)$generation[[1]], 
+                                     fitness.vec)$generation[[1]], 
                  c(1, 1, 1, 1, 1, 1, 1, 1, 1, 1))
     
     expect_equal(ReplaceClones(lapply(1:16, function(x) {rep(NA, times=10)}), 
-                                     fitness.vec, 10)$generation[[1]], 
+                                     fitness.vec)$generation[[1]], 
                  c(NA, NA, NA, NA, NA, NA, NA, NA, NA, NA))
     
   })
 
 test_that("check the dimension of return generation from ReplaceClones function is correct",{
     expect_length(ReplaceClones(lapply(1:16, function(x) {rep(1, times=10)}),
-                                rnorm(16, 500, 20), 10)$generation, 16)
+                                rnorm(16, 500, 20))$generation, 16)
   })
 
 

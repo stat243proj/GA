0ef5ebf405a3016804ea6724dbab89285f2e10c6
echo-orange <31430464+echo-orange@users.noreply.github.com>
2017-12-10 16:54:02 -0800

    Update Test_Select_Function.R

--- tests/testthat/Test_Select_Function.R
+++ tests/testthat/Test_Select_Function.R
@@ -6,5 +6,7 @@ context("Test the type of return value of Selection function")
   set.seed(7)
   output <- Select(baseball.dat, "salary", userfunc="AIC", user.family="gaussian", flag.log.scale=TRUE,
                    frac.replace=0.2, Niter=100, mutate.rate=FALSE, plot.flag=TRUE)
-    expect_equal(sum(output$bestModel$residuals), 1.062084e-15)
+   expect_equal(sum(output$bestModel$residuals), 1.062084e-15)
+   expect_equal(output$LastGen[[1]], 
+                 c(1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0))
   })

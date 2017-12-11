fe72a481cca33f0a6fa9584667865ccc28638830
ShanGao5 <31430282+ShanGao5@users.noreply.github.com>
2017-12-10 17:09:40 -0800

    Update Test_CrossOverMutate_Function.R

--- tests/testthat/Test_CrossOverMutate_Function.R
+++ tests/testthat/Test_CrossOverMutate_Function.R
@@ -1,25 +1,16 @@
 #Test the CrossOverMutate function
+data(mtcars)
+C <- dim(mtcars)[2] - 1
+P <- as.integer(C.cars*1.5)
+P <- 2*ceiling(P.cars/2)
+prob.mutate <- 1.0/(P*sqrt(C))
 context("Test the dimension of the output of CrossOverMutate function")
-
-#test CrossOverMutate function
- test_that("Check the value of return value(a child genome) from CrossOverMutate function is correct",{
-    expect_equal(CrossOverMutate(lapply(1:40, function(x) {rep(1, times=27)}), c(1,2), 0.004811252)[[1]], 
-    c(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1))
-  })
-
-test_that("Check the value of return value from CrossOverMutate function with a zero list is correct",{
-  expect_equal(CrossOverMutate(lapply(1:40, function(x) {rep(0, times=27)}), 
-                                 c(1,2), 0.004811252)[[1]], 
-    c(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0))
-  })
-
-test_that("Check the value of return value from CrossOverMutate function with an NA list is correct",{
-  expect_equal(CrossOverMutate(lapply(1:40, function(x) {rep(NA, times=27)}), 
-                                 c(1,2), 0.004811252)[[1]], 
-    as.numeric(c(NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA)))
-  })
-  
-  test_that("Check the dimension of return value(a child genome) from CrossOverMutate function is correct",{
-    generation <- lapply(1:40, function(x) {rep(1, times=27)})
-    expect_length(unlist(CrossOverMutate(generation, c(1,2), 0.004811252)), 54)
-  })
+test_that("Check the value of return value(a child genome) from CrossOverMutate function is correct",{
+  set.seed(2)
+  generation <- lapply(1:P, function(x) {rbinom(C, 1, 0.5)})
+  expect_equal(CrossOverMutate(generation, c(1,2), prob.mutate)[[1]], 
+               c(1, 0, 1, 0, 1, 1, 0, 1, 0, 0))
+  expect_equal(CrossOverMutate(generation, c(3,4), prob.mutate)[[1]], 
+               c(0, 0, 1, 0, 0, 1, 1, 0, 1, 0))
+  expect_length(unlist(CrossOverMutate(generation, c(5,6), prob.mutate)), 20)
+})

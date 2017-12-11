50c157c7de0e43e027ec6c57e477b4fc0326eeb2
ShanGao5 <31430282+ShanGao5@users.noreply.github.com>
2017-12-10 17:10:22 -0800

    Update Test_ExtractResponseVariable_Function.R

--- tests/testthat/Test_ExtractResponseVariable_Function.R
+++ tests/testthat/Test_ExtractResponseVariable_Function.R
@@ -1,11 +1,14 @@
-#Test the ExtractResponseVariable
+#Test the ExtractResponseVariable function
+data(mtcars)
+C <- dim(mtcars)[2] - 1
+P <- as.integer(C.cars*1.5)
+P <- 2*ceiling(P.cars/2)
+prob.mutate <- 1.0/(P*sqrt(C))
 context("Test the output of ExtractResponseVariable")
-
-test_ExtractResponseVariable <- function(dataset, response.name){
-  require(testthat)
-  
-  #test ExtractResponseVariable function
-  test_that("Check the length of baseball.dat response variables is 337",{
-    expect_equal(length(unlist(ExtractResponseVariable(baseball.dat, "salary")[[1]])), 337)
-  })
-}
+test_that("Check the length of dataset mtcars response variables is 32, 
+            and the first and third elements of salary are 21.0 and 22.8",{
+  data(mtcars)
+  expect_equal(length(unlist(ExtractResponseVariable(mtcars, "mpg")[[1]])), 32)
+  expect_equal(as.numeric((unlist(ExtractResponseVariable(mtcars, "mpg")[[1]]))[1]), 21.0)
+  expect_equal(as.numeric((unlist(ExtractResponseVariable(mtcars, "mpg")[[1]]))[3]), 22.8)
+})

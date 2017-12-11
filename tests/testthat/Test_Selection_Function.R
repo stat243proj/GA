2128b2a4fa5889db0617e60e472bd4359028876b
echo-orange <31430464+echo-orange@users.noreply.github.com>
2017-12-10 16:44:02 -0800

    Update Test_Selection_Function.R

--- tests/testthat/Test_Selection_Function.R
+++ tests/testthat/Test_Selection_Function.R
@@ -1,12 +1,10 @@
-#Test the Selection function
+#Test the Select function
 context("Test the type of return value of Selection function")
 
-test_Selection <- function(dataset, response.name){
-  require(testthat)
-  
-  #test Selection function
-  test_that("Check selection function",{
-    expect_equal(Selection(baseball.dat, "salary", userfunc="AIC", user.family="gaussian", flag.log.scale=TRUE,
-                           frac.replace=0.2, Niter=100, mutate.rate=FALSE, plot.flag=TRUE), )
+  #test Select function
+ test_that("Check the output of the Select function is correct",{
+  set.seed(7)
+  output <- Select(baseball.dat, "salary", userfunc="AIC", user.family="gaussian", flag.log.scale=TRUE,
+                   frac.replace=0.2, Niter=100, mutate.rate=FALSE, plot.flag=TRUE)
+    expect_equal(sum(output$bestModel$residuals), 1.062084e-15)
   })
-}

2ec320f2f431353a7eccb76595798475276d0c5b
ShanGao5 <31430282+ShanGao5@users.noreply.github.com>
2017-12-10 17:08:47 -0800

    Update Test_Breed_Function.R

--- tests/testthat/Test_Breed_Function.R
+++ tests/testthat/Test_Breed_Function.R
@@ -1,28 +1,14 @@
 #Test the Breed function
+data(mtcars)
+C <- dim(mtcars)[2] - 1
+P <- as.integer(C.cars*1.5)
+P <- 2*ceiling(P.cars/2)
+prob.mutate <- 1.0/(P*sqrt(C))
 context("Test the dimension and value of the output of Breed function")
-
 test_that("With the baseball data, check the output of the Breed function is correct",{
-              set.seed(1)
-              generation <- lapply(1:40, function(x) {rbinom(27, 1, 0.5)})
-              set.seed(1)
-              fitness.vec <- rnorm(40, 700, 1)
-              expect_equal(Breed(generation, fitness.vec, 0.004811252)[[1]], 
-               c(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1))
-})
-
-test_that("Check the output of the Breed function with NA input is correct",{
-expect_equal(Breed(lapply(1:40, function(x) {rep(NA, times=27)}),
-                                 fitness.vec, 0.004811252)[[1]],
-                           as.numeric(c(NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA)))
-})
-
-test_that("With the baseball data, check the dimension and length of return list from Breed function are correct",{
-              set.seed(1)
-              generation <- lapply(1:40, function(x) {rbinom(27, 1, 0.5)})
-              set.seed(1)
-              fitness.vec <- rnorm(40, 700, 1)
-              expect_length(Breed(generation, fitness.vec, 0.004811252), 40)
-              expect_equal(unique(unlist(
-                lapply(Breed(generation, fitness.vec, 0.004811252),function(x) length(unlist(x))
-                ))), 10)
+  set.seed(1)
+  generation <- lapply(1:P, function(x) {rbinom(C, 1, 0.5)})
+  fitness.vec <- rnorm(P, 160, 10)
+  expect_equal(Breed(generation, fitness.vec, prob.mutate)[[1]], 
+               c(1, 1, 0, 0, 1, 1, 1, 0, 1, 1))
 })

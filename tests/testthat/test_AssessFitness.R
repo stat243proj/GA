8c6493f12b1c38bbc7c299cc32297a589b99a56a
echo-orange <31430464+echo-orange@users.noreply.github.com>
2017-12-13 14:58:08 -0800

    Update test_AssessFitness.R

--- tests/testthat/test_AssessFitness.R
+++ tests/testthat/test_AssessFitness.R
@@ -4,10 +4,9 @@ C <- dim(mtcars)[2] - 1
 P <- as.integer(C*1.5)
 P <- 2*ceiling(P/2)
 prob.mutate <- 1.0/(P*sqrt(C))
-set.seed(1)
 context("Test the return value of AssessFitness function")
 test_that("Check the return value of AssessFitness function with mtcars data",{
-  individual <- rbinom(10, 1, 0.5)
+  individual <- c(0,1,0,1,0,0,1,0,1,1)
   expect_equal(floor(AssessFitness(individual, mtcars["mpg"], mtcars,
-                user.family = "gaussian", userfunc = "AIC")), 164)
+                user.family = "gaussian", userfunc = "AIC")), 166)
 })

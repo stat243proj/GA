341d10b5bae0b49add160a77589de4d367a51402
Kexin Fei <31425560+kexinfei@users.noreply.github.com>
2017-12-13 16:53:39 -0800

    Update ExtractResponseVariable.R

--- R/ExtractResponseVariable.R
+++ R/ExtractResponseVariable.R
@@ -16,29 +16,40 @@
 #' \code{\link[GA]{Select}}
 #'
 
-ExtractResponseVariable <- function(dataset, response.name) {
-
-  if (any(is.na(dataset[,1]))==TRUE){
-    print("The response variable has missing values")
-    dataset <- dataset[!is.na(dataset[,1])]
+ExtractResponseVariable <- function(dataset, name){
+  if (!is.data.frame(dataset)){
+    print("WARNING: The input data is not a data frame")
+    stop()
+    geterrmessage()
+   }
+  
+  #Check if column values are numeric
+  if(sum(!(sapply(dataset[, -which(names(dataset)==name)], class) %in% c("numeric","integer"))>0)){
+    print("WARNING: There exists certain non-numeric predictor(s)")
+    stop()
+    geterrmessage()
   }
-  else{
-    if (response.name %in% colnames(dataset)) {
-
-      response.name <- as.character(response.name)
-
-      #Get matrix of predictors
-      predictors <- dataset
-      predictors[response.name] <- NULL
-
-      #Get response variable
-      response <- dataset[response.name]
-      return(list(response,predictors))
-    } else {
-      print(paste("Name ", response.name," not found in dataset",sep=''))
-      return(list(0L,0L))
-    }
+  
+  #If there are missing values in response variable, remove the observation and proceed the function
+  if(any(is.na(dataset[name]))){
+    print("The response variable has missing values")
+    dataset <- dataset[!is.na(dataset[name])]
+  }
+  
+  #Check if the response variable is in the dataset
+  if(name %in% colnames(dataset)) {
+    
+    #Get matrix of predictors
+    predictors <- dataset
+    predictors[name] <- NULL
+    
+    #Get response variable
+    response <- dataset[name]
+    return(list(response,predictors))
+  } 
+  
+  else {
+    print(paste("Response variable ",name," not found in dataset",sep=''))
+    return(list(0L,0L))
   }
 }
-
-
